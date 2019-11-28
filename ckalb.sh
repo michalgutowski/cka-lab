97285d63232257e56247dcc2a4a4555dc8664107
Michal Gutowski <45829609+michalgutowski@users.noreply.github.com>
2019-11-28 21:28:27 +0100

    Adding nfs server with simple export

--- ckalb.sh
+++ ckalb.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-# Install haproxy
-apt install -y haproxy
+# Install haproxy and nfs server
+apt install -y haproxy nfs-kernel-server
 
 # Configure and enable haproxy
 cp -b /vagrant/haproxy/haproxy.cfg /etc/haproxy/
 systemctl start haproxy
 systemctl enable haproxy
+
+# Configure NFS server for persistent volume labs
+mkdir /opt/sfw
+chmod a+rwt /opt/sfw
+printf 'software' > /opt/sfw/hello.txt
+printf '/opt/sfw *(rw,no_root_squash,async,no_subtree_check,insecure)\n' > /etc/exports
+systemctl start nfs-kernel-server
+systemctl enable nfs-kernel-server
+
